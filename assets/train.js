a904db6238b40c0a893d9e3e5d86ee8d1be69762
Yahya Alhaddad <yahyaalhaddad@yahyas-mbp.home>
2018-03-16 16:14:54 -0400

    initial files

---
+++ assets/train.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/4.11.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCs4WVhzCj8lN3R-Nb1u7TZ9Injjzz64Nw",
+    authDomain: "train-schedule-923c3.firebaseapp.com",
+    databaseURL: "https://train-schedule-923c3.firebaseio.com",
+    projectId: "train-schedule-923c3",
+    storageBucket: "",
+    messagingSenderId: "565478800578"
+  };
+  firebase.initializeApp(config);
+</script>
